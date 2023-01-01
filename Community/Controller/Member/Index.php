6df1c257a3970ee0f8af49238c1d2ef3c13994b5
Dao Nguyen <dao.nv223@gmail.com>
2023-01-01 14:43:36 +0000

    Feature: Register and view info UI

---
+++ Community/Controller/Member/Index.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace DaoNguyen\Community\Controller\Member;
+
+use Magento\Customer\Controller\AbstractAccount;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends AbstractAccount implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * @return ResultInterface|null
+     */
+    public function execute(): ?ResultInterface
+    {
+        return $this->pageFactory->create();
+    }
+}
